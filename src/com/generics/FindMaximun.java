29c49704f905f030492747bf78cd9b90880218dc
akash <akashyewale007@gmail.com>
2023-03-08 12:57:07 +0530

    find maximum float

--- src/com/generics/FindMaximun.java
+++ src/com/generics/FindMaximun.java
@@ -26,6 +26,17 @@ public class FindMaximun {
 		}
 		System.out.println("max float is : "+max);
 	}
+	void getmaxString(String n1, String n2, String n3) {
+		String max=n1;
+		if(n2.compareTo(max)>0) {
+			max=n2;
+			
+		}
+		if(n3.compareTo(max)>0) {
+			max=n3;
+		}
+		System.out.println("maximum string is :" +max);
+	}
 
 	public static void main(String[] args) {
 		System.out.println(" welcome to generics ");
@@ -33,6 +44,7 @@ public class FindMaximun {
 		FindMaximun obj=new FindMaximun();
 		obj.getmaxno(3, 1, 2);
 		obj.getmaxflot(23.4f, 1.2f, 23.5f);
+		obj.getmaxString("akash", "abhishekh", "arpit");
 
 	}
 
